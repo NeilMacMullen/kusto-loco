596dc9784774c1f0199214ca338b7a7880e1b303
David Nissimoff <davidni@microsoft.com>
2021-10-12 16:21:25 -0700

    Added wip ITabularSourceV2 for Tommy

--- KustoExecutionEngine.Core/ITabularSource.cs
+++ KustoExecutionEngine.Core/ITabularSource.cs
@@ -9,4 +9,63 @@
     {
         object? this[string columnName] { get; }
     }
+
+
+
+    // TODO Tommy:
+    public interface ITabularSourceV2
+    {
+        ITableSchema Schema { get; }
+
+        ITableChunk GetNextChunk();
+    }
+
+    public interface ITableChunk
+    {
+        List<List<KustoValue>> ColumnValues { get; }
+    }
+
+    public interface ITableSchema
+    {
+        List<ColumnDefinition> ColumnDefinitions { get; }
+
+        int GetColumnIndex(string columnName);
+    }
+
+    public interface ColumnDefinition
+    {
+        string ColumnName { get; }
+        KustoValueKind ValueType { get; }
+    }
+
+    public struct KustoValue
+    {
+        KustoValueKind ValueType { get; }
+        object Value { get; }
+    }
+
+    public enum KustoValueKind
+    {
+        Bool,
+
+        Int,
+
+        Long,
+
+        Real,
+
+        Decimal,
+
+        String,
+
+        DateTime,
+
+        TimeSpan,
+
+        Guid,
+
+        Type,
+
+        Dynamic
+    }
 }
