0c984cf130fd8fff90db69254325083bc9c74228
David Nissimoff <davidni@microsoft.com>
2021-10-12 08:19:23 -0700

    Initial commit

---
+++ KustoExecutionEngine.Cli/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using KustoExecutionEngine.Core;
+
+var engine = new KqlEngine();
+
+engine.DumpTree(@"
+let d=(10.0 + 1)*todouble(1);
+MyTable
+| project a = a + b, c = 1
+| where a > d");
+
+engine.DumpTree(@"
+MyTable
+| where a > 1.0");
+
+engine.DumpTree(@"
+print 'hello'");
+
+Console.WriteLine("Done");
---
+++ KustoExecutionEngine.Core/KqlEngine.cs
@@ -0,0 +1,65 @@
+﻿using Kusto.Language;
+using Kusto.Language.Symbols;
+using Kusto.Language.Syntax;
+
+namespace KustoExecutionEngine.Core
+{
+    public class KqlEngine
+    {
+        private GlobalState _globals;
+
+        public KqlEngine()
+        {
+            var db = new DatabaseSymbol(
+                    "MyDb",
+                    new[]
+                    {
+                        new TableSymbol("MyTable", "(a: real, b: real)"),
+                    });
+            _globals = GlobalState.Default.WithDatabase(db);
+        }
+
+        public void Foo()
+        {
+            var query = "MyTable | project a = a + b | where a > 10.0";
+            var code = KustoCode.ParseAndAnalyze(query, _globals);
+
+            // search syntax tree for references to specific columns
+            var columnA = _globals.Database.Tables.First(t => t.Name == "MyTable").GetColumn("a");
+            var referencesToA = code.Syntax.GetDescendants<NameReference>(n => n.ReferencedSymbol == columnA);
+
+            foreach (var reference in referencesToA)
+            {
+                Console.WriteLine(reference.ToString());
+            }
+        }
+
+        public void DumpTree(string query)
+        {
+            Console.WriteLine("Analyzing query:");
+            Console.WriteLine(query);
+            Console.WriteLine();
+
+            LetStatement a;
+
+            var code = KustoCode.ParseAndAnalyze(query, _globals);
+
+            int indent = 0;
+            SyntaxElement.WalkNodes(
+                code.Syntax,
+                fnBefore: node =>
+                {
+                    Console.Write(new string(' ', indent));
+                    Console.WriteLine($"{node.Kind} ({TypeNameHelper.GetTypeDisplayName(node.GetType())}): {node.ToString(IncludeTrivia.SingleLine)}");
+                    indent++;
+                },
+                fnAfter: node =>
+                {
+                    indent--;
+                });
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
