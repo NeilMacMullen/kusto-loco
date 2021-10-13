c3c17ebb84ec5f6c8cb5c1da3b33754211cca843
David Nissimoff <davidni@microsoft.com>
2021-10-13 11:27:50 -0700

    Remove dead code

--- KustoExecutionEngine.Core/Expressions/Operators/StirlingOperator.cs
+++ KustoExecutionEngine.Core/Expressions/Operators/StirlingOperator.cs
@@ -1,5 +1,4 @@
 ﻿using Kusto.Language.Syntax;
-using KustoExecutionEngine.Core.Expressions;
 
 namespace KustoExecutionEngine.Core.Expressions.Operators
 {
@@ -17,20 +16,5 @@ namespace KustoExecutionEngine.Core.Expressions.Operators
         }
 
         protected abstract object EvaluateInternal(ITabularSource input);
-    }
-
-    internal static class StirlingOperator
-    {
-        internal static StirlingExpression Build(StirlingEngine engine, QueryOperator @operator)
-        {
-            return @operator.Kind switch
-            {
-                SyntaxKind.FilterOperator => new StirlingFilterOperator(engine, (FilterOperator)@operator),
-                SyntaxKind.SummarizeOperator => new StirlingSummarizeOperator(engine, (SummarizeOperator)@operator),
-                SyntaxKind.ProjectOperator => new StirlingProjectOperator(engine, (ProjectOperator)@operator),
-                SyntaxKind.JoinOperator => new StirlingJoinOperator(engine, (JoinOperator)@operator),
-                _ => throw new InvalidOperationException($"Unsupported operator kind '{@operator.Kind}'."),
-            };
-        }
     }
 }
