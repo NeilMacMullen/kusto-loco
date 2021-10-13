81e0d490e4aa258f2101ee91c050cfd8c69fcbdb
David Nissimoff <davidni@microsoft.com>
2021-10-13 12:41:23 -0700

    Function calls starting to work

--- KustoExecutionEngine.Core/Expressions/StirlingFunctionCallExpression.cs
+++ KustoExecutionEngine.Core/Expressions/StirlingFunctionCallExpression.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Kusto.Language;
 using Kusto.Language.Syntax;
 
@@ -9,16 +11,36 @@ namespace KustoExecutionEngine.Core.Expressions
         private delegate object Impl(object? input);
         private readonly Impl _impl;
 
+        private readonly StirlingExpression[] _argumentExpressions;
+
         public StirlingFunctionCallExpression(StirlingEngine engine, FunctionCallExpression expression)
             : base(engine, expression)
         {
+            _argumentExpressions = expression.ArgumentList.Expressions
+                .Select(e => StirlingExpression.Build(engine, e.Element))
+                .ToArray();
+
             if (ReferenceEquals(expression.ReferencedSymbol, Functions.ToLong))
             {
+                CheckExpectedArgumentsCount(1);
                 _impl = ToLongImpl;
+            }
+            else if (ReferenceEquals(expression.ReferencedSymbol, Aggregates.Count))
+            {
+                CheckExpectedArgumentsCount(0);
+                _impl = CountImpl;
             }
             else
             {
                 throw new InvalidOperationException($"Unsupported function {expression}.");
+            }
+
+            void CheckExpectedArgumentsCount(int expected)
+            {
+                if (_argumentExpressions.Length != expected)
+                {
+                    throw new InvalidOperationException($"Mismatched arguments count. Function {expression.Name} expects {expected}, found {_argumentExpressions.Length}.");
+                }
             }
         }
 
@@ -27,11 +49,20 @@ namespace KustoExecutionEngine.Core.Expressions
             return _impl(input);
         }
 
-        private static object ToLongImpl(object? input)
+        private object ToLongImpl(object? input)
         {
-            //var value = 
-            // TODO: Handle all possible input scalar data types
-            return Convert.ToInt64(input);
+            var argValue = _argumentExpressions[0].Evaluate(input);
+            return Convert.ToInt64(argValue);
+        }
+
+        private object CountImpl(object? input)
+        {
+            if (input is IList<IRow> table)
+            {
+                return table.Count;
+            }
+
+            throw new NotSupportedException($"Unexpected input type, expected IList<IRow>, got {TypeNameHelper.GetTypeDisplayName(input)}.");
         }
     }
 }
