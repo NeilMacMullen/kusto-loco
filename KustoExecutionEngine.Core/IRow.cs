61058b455862e7a97b8dd4ec4344fce716af7e2e
David Nissimoff <davidni@microsoft.com>
2021-10-13 21:50:02 -0700

    Removed old data structures

---
+++ KustoExecutionEngine.Core/IRow.cs
@@ -0,0 +1,11 @@
+﻿using KustoExecutionEngine.Core.DataSource;
+
+namespace KustoExecutionEngine.Core
+{
+    public interface IRow
+    {
+        TableSchema Schema { get; }
+
+        object?[] Values { get; }
+    }
+}
