032baff2ca893a0f8c3f3026878079e8b436dd56
Vicky Li <vickyli@microsoft.com>
2021-10-14 16:33:56 -0700

    Fixed renaming gap

--- src/BabyKusto.BlazorApp/Pages/Error.cshtml.cs
+++ src/BabyKusto.BlazorApp/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace BlazorApp.Pages;
+namespace BabyKusto.BlazorApp.Pages;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
