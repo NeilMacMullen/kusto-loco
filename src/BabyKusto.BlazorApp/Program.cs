343b1cb0782339645ddf448de197fe014ddc8a24
Vicky Li <vickyli@microsoft.com>
2021-10-14 20:00:05 -0700

    Added dynamic query feature.

--- src/BabyKusto.BlazorApp/Program.cs
+++ src/BabyKusto.BlazorApp/Program.cs
@@ -1,13 +1,11 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using BabyKusto.BlazorApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<FileProcessor>();
 
 var app = builder.Build();
 
