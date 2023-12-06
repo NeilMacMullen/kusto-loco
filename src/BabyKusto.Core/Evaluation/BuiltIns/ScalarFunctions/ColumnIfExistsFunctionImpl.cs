850a7dbb37935200791842c81dad859ba2c23421
Neil MacMullen <neil.macmullen@outlook.com>
2023-12-06 21:57:05 +0000

    Use central package management

---
+++ src/BabyKusto.Core/Evaluation/BuiltIns/ScalarFunctions/ColumnIfExistsFunctionImpl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BabyKusto.Core.Evaluation.BuiltIns.Impl;
+
+internal class ColumnIfExistsFunctionImpl : IScalarFunctionImpl
+{
+    public ScalarResult InvokeScalar(ScalarResult[] arguments) => throw new NotImplementedException();
+
+    public ColumnarResult InvokeColumnar(ColumnarResult[] arguments)
+    {
+        var columnName = arguments[0];
+        throw new NotImplementedException();
+    }
+}
