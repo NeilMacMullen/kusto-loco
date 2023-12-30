84e1e984baeba2d0bbdd3b3080e3cd0f5806b670
Neil MacMullen <neil.macmullen@outlook.com>
2023-12-30 13:33:49 +0000

    Add reverse type lookup in TypeMapping

--- src/BabyKusto.Core/Util/TypeMapping.cs
+++ src/BabyKusto.Core/Util/TypeMapping.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json.Nodes;
 using Kusto.Language.Symbols;
 
@@ -21,10 +22,25 @@ public static class TypeMapping
         [typeof(JsonNode)] = ScalarTypes.Dynamic,
     };
 
+
+    private static readonly Dictionary<TypeSymbol, Type> KustoToNetToLookup;
+
+    static TypeMapping()
+    {
+        KustoToNetToLookup = NetToKustoLookup.ToDictionary(kv => kv.Value, kv => kv.Key);
+    }
+
     public static TypeSymbol SymbolForType(Type type)
     {
         if (NetToKustoLookup.TryGetValue(type, out var ts))
             return ts;
         throw new NotImplementedException($"No TypeSymbol equivalent for .Net type {type.Name}");
+    }
+
+    public static Type TypeForSymbol(TypeSymbol ts)
+    {
+        if (KustoToNetToLookup.TryGetValue(ts, out var type))
+            return type;
+        throw new NotImplementedException($"No .Net type  equivalent for typeSymbol {ts.Name}");
     }
 }
