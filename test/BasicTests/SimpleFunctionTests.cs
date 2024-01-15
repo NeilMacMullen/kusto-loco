015cd0a41adc598840e57dceb95f654c3525ad3c
Neil MacMullen <neil.macmullen@outlook.com>
2024-01-15 17:16:58 +0000

    Clean up warnings

--- test/BasicTests/SimpleFunctionTests.cs
+++ test/BasicTests/SimpleFunctionTests.cs
@@ -1,4 +1,5 @@
 using FluentAssertions;
+// ReSharper disable StringLiteralTypo
 
 namespace BasicTests;
 
@@ -8,7 +9,7 @@ public class SimpleFunctionTests : TestMethods
     [TestMethod]
     public async Task TrimStart()
     {
-        var query = @"print trim_start(@'a+','aaainnerbbba')";
+        var query = "print trim_start(@'a+','aaainnerbbba')";
         var result = await LastLineOfResult(query);
         result.Should().Be("innerbbba");
     }
@@ -16,7 +17,7 @@ public class SimpleFunctionTests : TestMethods
     [TestMethod]
     public async Task TrimEnd()
     {
-        var query = @"print trim_end(@'b+','baaainnerbbb')";
+        var query = "print trim_end(@'b+','baaainnerbbb')";
         var result = await LastLineOfResult(query);
         result.Should().Be("baaainner");
     }
@@ -24,7 +25,7 @@ public class SimpleFunctionTests : TestMethods
     [TestMethod]
     public async Task Trim()
     {
-        var query = @"print trim(@'a+','aaaainneraaaa')";
+        var query = "print trim(@'a+','aaaainneraaaa')";
         var result = await LastLineOfResult(query);
         result.Should().Be("inner");
     }
@@ -56,7 +57,7 @@ datatable(Size:int) [50]
     [TestMethod]
     public async Task GeoDistance2PointsScalar()
     {
-        var query = @" print distance_in_meters = geo_distance_2points(-122.407628, 47.578557, -118.275287, 34.019056)";
+        var query = " print distance_in_meters = geo_distance_2points(-122.407628, 47.578557, -118.275287, 34.019056)";
         var result = await LastLineOfResult(query);
         result.Should().Contain("15467");
     }
@@ -64,7 +65,7 @@ datatable(Size:int) [50]
     [TestMethod]
     public async Task GeoPointToGeoHashScalar()
     {
-        var query = @"print geohash = geo_point_to_geohash(139.806115, 35.554128, 12)";
+        var query = "print geohash = geo_point_to_geohash(139.806115, 35.554128, 12)";
         var result = await LastLineOfResult(query);
         result.Should().Contain("xn76m27ty9g4");
     }
@@ -72,7 +73,7 @@ datatable(Size:int) [50]
     [TestMethod]
     public async Task GeoPointToGeoHashScalarWithDefault()
     {
-        var query = @"print geohash = geo_point_to_geohash(139.806115, 35.554128)";
+        var query = "print geohash = geo_point_to_geohash(139.806115, 35.554128)";
         var result = await LastLineOfResult(query);
         result.Should().Be("xn76m");
     }
@@ -92,7 +93,7 @@ datatable(Size:int) [50]
     [TestMethod]
     public async Task SplitScalar()
     {
-        var query = @"print c=split('this.is.a.string.and.I.need.the.last.part', '.')[-1]";
+        var query = "print c=split('this.is.a.string.and.I.need.the.last.part', '.')[-1]";
         var result = await LastLineOfResult(query);
         result.Should().Be("part");
     }
@@ -101,7 +102,7 @@ datatable(Size:int) [50]
     [TestMethod]
     public async Task ToLower()
     {
-        var query = @"print c=tolower('ABCdef')";
+        var query = "print c=tolower('ABCdef')";
         var result = await LastLineOfResult(query);
         result.Should().Be("abcdef");
     }
@@ -109,7 +110,7 @@ datatable(Size:int) [50]
     [TestMethod]
     public async Task ToUpper()
     {
-        var query = @"print c=toupper('ABCdef')";
+        var query = "print c=toupper('ABCdef')";
         var result = await LastLineOfResult(query);
         result.Should().Be("ABCDEF");
     }
@@ -117,7 +118,7 @@ datatable(Size:int) [50]
     [TestMethod]
     public async Task Strlen()
     {
-        var query = @"print c=strlen('ABCdef')";
+        var query = "print c=strlen('ABCdef')";
         var result = await LastLineOfResult(query);
         result.Should().Be("6");
     }
@@ -125,7 +126,7 @@ datatable(Size:int) [50]
     [TestMethod]
     public async Task SubString()
     {
-        var query = @"print c=substring('ABCdef',2,3)";
+        var query = "print c=substring('ABCdef',2,3)";
         var result = await LastLineOfResult(query);
         result.Should().Be("Cde");
     }
@@ -133,7 +134,7 @@ datatable(Size:int) [50]
     [TestMethod]
     public async Task Trimws()
     {
-        var query = @"print c=trimws('   abc   ')";
+        var query = "print c=trimws('   abc   ')";
         var result = await LastLineOfResult(query);
         result.Should().Be("abc");
     }
@@ -141,7 +142,7 @@ datatable(Size:int) [50]
     [TestMethod]
     public async Task DateTimeBin()
     {
-        var query = @"print bin(datetime(1970-05-11 13:45:07), 1d)";
+        var query = "print bin(datetime(1970-05-11 13:45:07), 1d)";
         var result = await LastLineOfResult(query);
         result.Should().Be("1970-05-11 00:00:00Z");
     }
@@ -149,7 +150,7 @@ datatable(Size:int) [50]
     [TestMethod]
     public async Task TimespanFormatting()
     {
-        var query = @"print 1d";
+        var query = "print 1d";
         var result = await LastLineOfResult(query);
         result.Should().Be("1.00:00:00");
     }
@@ -157,7 +158,7 @@ datatable(Size:int) [50]
     [TestMethod]
     public async Task DateTimeToLocal()
     {
-        var query = @"print datetime_utc_to_local(datetime(2015-12-31 23:59:59.9), 'US/Eastern')";
+        var query = "print datetime_utc_to_local(datetime(2015-12-31 23:59:59.9), 'US/Eastern')";
         var result = await LastLineOfResult(query);
         result.Should().Be("2015-12-31 18:59:59.9000");
     }
@@ -166,7 +167,7 @@ datatable(Size:int) [50]
     [TestMethod]
     public async Task Range()
     {
-        var query = @"range i from 1 to 10 step 1";
+        var query = "range i from 1 to 10 step 1";
         var result = await LastLineOfResult(query);
         result.Should().Be("10");
     }
@@ -174,7 +175,7 @@ datatable(Size:int) [50]
     [TestMethod]
     public async Task RangeDescending()
     {
-        var query = @"range i from 10 to 1 step -1";
+        var query = "range i from 10 to 1 step -1";
         var result = await LastLineOfResult(query);
         result.Should().Be("1");
     }
@@ -183,7 +184,7 @@ datatable(Size:int) [50]
     [TestMethod]
     public async Task RowNumberNoParam()
     {
-        var query = @"range i from 10 to 20 step 1 | extend r =row_number()";
+        var query = "range i from 10 to 20 step 1 | extend r =row_number()";
         var result = await LastLineOfResult(query);
         result.Should().Be("20,10");
     }
@@ -191,7 +192,7 @@ datatable(Size:int) [50]
     [TestMethod]
     public async Task RowNumberStartingAt7()
     {
-        var query = @"range i from 1 to 5 step 1 | extend r =row_number(7)";
+        var query = "range i from 1 to 5 step 1 | extend r =row_number(7)";
         var result = await LastLineOfResult(query);
         result.Should().Be("5,11");
     }
@@ -306,7 +307,7 @@ range x from datetime(2023-01-01) to datetime(2023-01-30) step 1d
     }
 
     [TestMethod]
-    public async Task BetweenDateTimeTimepan()
+    public async Task BetweenDateTimeTimespan()
     {
         //ensure we didn't get any fractional values
         var query = @"
@@ -331,7 +332,7 @@ range x from datetime(2023-01-01) to datetime(2023-01-30) step 1d
     public async Task HasAny()
     {
         //ensure we didn't get any fractional values
-        var query = @"print x='line1' | where x has_any('a','b')";
+        var query = "print x='line1' | where x has_any('a','b')";
         var result = await LastLineOfResult(query);
         result.Should().Be("8");
     }
@@ -342,7 +343,7 @@ range x from datetime(2023-01-01) to datetime(2023-01-30) step 1d
     public async Task Arg_max()
     {
         //ensure we didn't get any fractional values
-        var query = @"print x=1,y=2 | summarize arg_max(x,*) by y"
+        var query = "print x=1,y=2 | summarize arg_max(x,*) by y"
             ;
         var result = await LastLineOfResult(query);
         result.Should().Be("8");
@@ -351,7 +352,7 @@ range x from datetime(2023-01-01) to datetime(2023-01-30) step 1d
     [TestMethod]
     public async Task LogTest()
     {
-        var query = @"print v1 = log(4.5)";
+        var query = "print v1 = log(4.5)";
         var result = await LastLineOfResult(query);
         result.Should().StartWith("1.504");
     }
@@ -359,7 +360,7 @@ range x from datetime(2023-01-01) to datetime(2023-01-30) step 1d
     [TestMethod]
     public async Task LogTest2()
     {
-        var query = @"datatable(a:real) [4.5,4.5] | project v1 = log(a)";
+        var query = "datatable(a:real) [4.5,4.5] | project v1 = log(a)";
         var result = await LastLineOfResult(query);
         result.Should().StartWith("1.504");
     }
@@ -367,7 +368,7 @@ range x from datetime(2023-01-01) to datetime(2023-01-30) step 1d
     [TestMethod]
     public async Task StrLen()
     {
-        var query = @"datatable(a:string) [''] | project v1 = strlen(a)";
+        var query = "datatable(a:string) [''] | project v1 = strlen(a)";
         var result = await LastLineOfResult(query);
         result.Should().Be("0");
     }
@@ -376,7 +377,7 @@ range x from datetime(2023-01-01) to datetime(2023-01-30) step 1d
     [TestMethod]
     public async Task ToDateTime()
     {
-        var query = @"print D=todatetime('15/01/2024 12:35:35')";
+        var query = "print D=todatetime('15/01/2024 12:35:35')";
         var result = await LastLineOfResult(query);
         result.Should().Be("2024-01-15 12:35:35Z");
     }
@@ -384,7 +385,7 @@ range x from datetime(2023-01-01) to datetime(2023-01-30) step 1d
     [TestMethod]
     public async Task ToDateTime2()
     {
-        var query = @"print D=todatetime('2024/01/15 12:35:35')";
+        var query = "print D=todatetime('2024/01/15 12:35:35')";
         var result = await LastLineOfResult(query);
         result.Should().Be("2024-01-15 12:35:35Z");
     }
@@ -394,7 +395,7 @@ range x from datetime(2023-01-01) to datetime(2023-01-30) step 1d
     [TestMethod]
     public async Task ToDateTimeFmt()
     {
-        var query = @"print D=todatetimefmt('2024-01-15 12:35:35','yyyy-MM-dd HH:mm:ss')";
+        var query = "print D=todatetimefmt('2024-01-15 12:35:35','yyyy-MM-dd HH:mm:ss')";
         var result = await LastLineOfResult(query);
         result.Should().Be("2024-01-15 12:35:35Z");
     }
