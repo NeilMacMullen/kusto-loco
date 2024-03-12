db67bf32882175d7ba1570e138b68bfd79fb79e0
Neil MacMullen <neil.macmullen@outlook.com>
2024-03-12 10:58:47 +0000

    Fix up project files

--- libraries/KustoLoco.Core/Util/TypeSymbolExtensions.cs
+++ libraries/KustoLoco.Core/Util/TypeSymbolExtensions.cs
@@ -6,10 +6,10 @@ using Kusto.Language.Symbols;
 
 namespace KustoLoco.Core.Extensions;
 
-internal static class TypeSymbolExtensions
+public static class TypeSymbolExtensions
 {
     [return: NotNullIfNotNull("type")]
-    internal static TypeSymbol? Simplify(this TypeSymbol? type) =>
+    public static TypeSymbol? Simplify(this TypeSymbol? type) =>
         type == null ? null :
         type.Name == ScalarTypes.Dynamic.Name ? ScalarTypes.Dynamic :
         type;
