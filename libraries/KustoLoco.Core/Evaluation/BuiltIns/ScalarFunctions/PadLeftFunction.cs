d388c8de5b3ec6fb986f95a73e890541a3992a99
Neil MacMullen <neil.macmullen@outlook.com>
2024-09-27 10:26:10 +0100

    - Update nuget dependencies - introduce sustring overload - intrdoduce padleft and padright

---
+++ libraries/KustoLoco.Core/Evaluation/BuiltIns/ScalarFunctions/PadLeftFunction.cs
@@ -0,0 +1,8 @@
+﻿namespace KustoLoco.Core.Evaluation.BuiltIns.Impl;
+
+[KustoImplementation(Keyword = "padleft")]
+internal partial class PadLeftFunction
+{
+    private static string Impl(string s,long n) => s.PadLeft((int)n,' ');
+    private static string WithCharImpl(string s,long n,string pad) => s.PadLeft((int)n,pad.Length >0 ? pad[0]:' ');
+}
--- libraries/KustoLoco.Core/Evaluation/BuiltIns/ScalarFunctions/SubstringFunction.cs
+++ libraries/KustoLoco.Core/Evaluation/BuiltIns/ScalarFunctions/SubstringFunction.cs
@@ -6,12 +6,17 @@ namespace KustoLoco.Core.Evaluation.BuiltIns.Impl;
 [KustoImplementation(Keyword = "Functions.Substring")]
 internal partial class SubstringFunction
 {
-    private static string Impl(string input, long start, long length)
+    internal static string Impl(string input, long start, long length)
     {
         var effectiveStart = Math.Max(0, Math.Min(start, input.Length));
         var maxAllowableLength = input.Length - effectiveStart;
         var effectiveLength =
             Math.Max(0, Math.Min(length, maxAllowableLength));
         return input.Substring((int)effectiveStart, (int)effectiveLength);
+    }
+
+    private static string AImpl(string input, long start)
+    {
+        return SubstringFunction.Impl(input, start, int.MaxValue);
     }
 }
