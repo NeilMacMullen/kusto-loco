8031caa2a5090d446cff4da3e44ad6e0655223e7
Neil MacMullen <neil.macmullen@outlook.com>
2025-01-01 17:49:41 +0000

    Tweak sleep command implemenation

--- libraries/lokql-engine/Commands/SleepCommand.cs
+++ libraries/lokql-engine/Commands/SleepCommand.cs
@@ -8,11 +8,13 @@ public static class SleepCommand
     {
         econtext.Explorer.Info("Sleeping...");
         await Task.Delay(TimeSpan.FromSeconds(o.Seconds));
+        econtext.Explorer.Info("Finished.");
     }
 
-    [Verb("sleep", HelpText = "render last results as html and save in active report")]
+    [Verb("sleep", HelpText = @"Sleep for a number of seconds (default is 1)
+Primarily used for testing but could be used for a basic slide-show")]
     internal class Options
     {
-        [Value(0)] public double Seconds { get; set; } =0;
+        [Value(0)] public double Seconds { get; set; } =1;
     }
 }
