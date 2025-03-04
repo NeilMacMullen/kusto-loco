4d054c228cbb09a1836356d9ee02870d42d0014a
Derek Gooding <djgooding@live.com>
2025-03-04 15:03:53 -0500

    cleanup empty line

--- samples/ProcessesWpf/ViewModels/Pages/DashboardViewModel.cs
+++ samples/ProcessesWpf/ViewModels/Pages/DashboardViewModel.cs
@@ -14,7 +14,6 @@ public partial class DashboardViewModel : ObservableObject, INavigationAware
     [ObservableProperty]
     private ObservableCollection<Object> _resultData = [];
 
-
     public Task OnNavigatedFromAsync() => Task.CompletedTask;
     public Task OnNavigatedToAsync() => Task.CompletedTask;
 
