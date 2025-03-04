7ca75193f748ddb5fa84538e7d0f1106e01451ec
Derek Gooding <djgooding@live.com>
2025-03-04 15:02:14 -0500

    Moved backend code to viewmodel and bind approrpiately.

--- samples/ProcessesWpf/ViewModels/Pages/DashboardViewModel.cs
+++ samples/ProcessesWpf/ViewModels/Pages/DashboardViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.ObjectModel;
+﻿using KustoLoco.Core;
+using System.Collections.Immutable;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Windows.Data;
 using Wpf.Ui.Abstractions.Controls;
 
@@ -6,6 +9,38 @@ namespace ProcessesWpf.ViewModels.Pages;
 
 public partial class DashboardViewModel : ObservableObject, INavigationAware
 {
+    [ObservableProperty]
+    private string _query = string.Empty;
+    [ObservableProperty]
+    private ObservableCollection<Object> _resultData = [];
+
+
     public Task OnNavigatedFromAsync() => Task.CompletedTask;
     public Task OnNavigatedToAsync() => Task.CompletedTask;
+
+    [RelayCommand]
+    private async Task RunAsync()
+    {
+        //get data into context
+        var context = new KustoQueryContext();
+
+        var processes = Process.GetProcesses()
+            .Select(p => new ProcessInfo(Pid: p.Id,
+                                         Name: p.ProcessName,
+                                         NumThreads: p.Threads.Count,
+                                         WorkingSet: p.WorkingSet64))
+            .ToImmutableArray();
+
+        context.WrapDataIntoTable("processes", processes);
+
+        //run query
+        var query = Query.Trim();
+        var result = await context.RunQuery(query);
+
+        //render results
+        foreach(var item in result.ToDataTableOrError().DefaultView)
+        {
+            ResultData.Add(item);
+        }
+    }
 }
--- samples/ProcessesWpf/Views/Pages/DashboardPage.xaml.cs
+++ samples/ProcessesWpf/Views/Pages/DashboardPage.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using KustoLoco.Core;
 using ProcessesWpf.ViewModels.Pages;
-using System.Collections.Immutable;
 using System.Diagnostics;
 using Wpf.Ui.Abstractions.Controls;
 
@@ -15,27 +14,5 @@ public partial class DashboardPage : INavigableView<DashboardViewModel>
         DataContext = this;
 
         InitializeComponent();
-    }
-
-    private async void OnRunButtonClicked(object sender, RoutedEventArgs e)
-    {
-        //get data into context
-        var context = new KustoQueryContext();
-
-        var processes = Process.GetProcesses()
-            .Select(p => new ProcessInfo(Pid: p.Id,
-                                         Name: p.ProcessName,
-                                         NumThreads: p.Threads.Count,
-                                         WorkingSet: p.WorkingSet64))
-            .ToImmutableArray();
-
-        context.WrapDataIntoTable("processes", processes);
-
-        //run query
-        var query = QueryEntry.Text.Trim();
-        var result = await context.RunQuery(query);
-
-        //render results
-        ResultDataGrid.ItemsSource = result.ToDataTableOrError().DefaultView;
     }
 }
