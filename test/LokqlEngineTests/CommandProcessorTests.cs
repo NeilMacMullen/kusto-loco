9adf7405c227cab2931e626336660b8dc65866c1
* <tbui17@users.noreply.github.com>
2025-04-04 17:05:27 -0600

    #171 Initial impl for supporting all file IO commands in path autocompletion

---
+++ test/LokqlEngineTests/CommandProcessorTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Lokql.Engine.Commands;
+
+namespace LokqlEngineTests;
+
+[TestClass]
+public class CommandProcessorTests
+{
+    [TestMethod]
+    public void GetVerbsOfOptionsThatTakeFilesAsInputDoesWhatItsNameDescribes()
+    {
+        var processor = CommandProcessor.Default();
+
+        var result = processor.GetVerbsOfOptionsThatTakeFilesAsInput();
+
+        result
+            .Should()
+            .ContainSingle(x => x.Equals("load", StringComparison.OrdinalIgnoreCase))
+            .And.NotContain(x => x.Equals("synonym", StringComparison.OrdinalIgnoreCase)); // SynTableCommand
+    }
+}
