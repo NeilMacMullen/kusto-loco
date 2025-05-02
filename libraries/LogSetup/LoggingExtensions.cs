311eb073ea2b5d5085d290f53ebdfd4ba0a975b9
* <tbui17@users.noreply.github.com>
2025-05-02 13:34:12 -0600

    feat: dependency injection, logging, top level error handling
    Integrate DI and logging
    
    Used ProcessesWPF sample app for reference
    
    Serilog logging extension
    
    Update logging config

--- libraries/LogSetup/LoggingExtensions.cs
+++ libraries/LogSetup/LoggingExtensions.cs
@@ -1,6 +1,14 @@
-﻿using NLog;
+﻿using System.Reflection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Formatting.Compact;
+using NLog;
 using NLog.Config;
 using NLog.Targets;
+using Serilog.Templates;
+using Serilog.Templates.Themes;
+using LogLevel = NLog.LogLevel;
+
 
 namespace LogSetup;
 
@@ -57,8 +65,57 @@ public static class LoggingExtensions
             if (!loggingTargets.Contains(LoggingTarget.Debugger))
                 return;
 
-            var debuggerTarget = new DebuggerTarget("debug") { Layout = "${time}|${message} ${exception}" };
+            var debuggerTarget = new DebuggerTarget("debug") { Layout = "${time}|${message} ${exception}", };
             config.LoggingRules.Add(new LoggingRule("*", LogLevel.Info, LogLevel.Fatal, debuggerTarget));
         }
     }
+
+    public static IHostApplicationBuilder UseApplicationLogging(this IHostApplicationBuilder builder)
+    {
+        var services = builder.Services;
+        Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+        var entryAssembly = Assembly.GetEntryAssembly();
+        var appName = entryAssembly?.GetName().Name ?? "Unknown";
+        var basePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), appName);
+        var logPath = Path.Combine(basePath, "logs.json");
+        Log.Information("Application storage path: {Path}", basePath);
+
+        services
+            .AddSerilog((sc, logBuilder) =>
+                {
+
+                    logBuilder
+                        .ReadFrom.Configuration(builder.Configuration)
+                        .ReadFrom.Services(sc)
+                        .Enrich.FromLogContext();
+#if DEBUG
+
+
+                    var newLine = Environment.NewLine;
+                    var template = new ExpressionTemplate(
+                        "[{@t:HH:mm:ss.fff} {@l:u3}] {Concat('\e[32m',Substring(SourceContext, LastIndexOf(SourceContext, '.') + 1))} | {@mt}{NewLine}  {@p}{NewLine}{@x}"
+                            .Replace("{NewLine}", newLine),
+                        theme: GetTheme(),
+                        applyThemeWhenOutputIsRedirected: true
+                    );
+                    logBuilder
+                        .WriteTo.Console(template)
+                        .WriteTo.File(new CompactJsonFormatter(), logPath, fileSizeLimitBytes: 5 * 1000 ^ 2);
+#else
+                    logBuilder.MinimumLevel.Information();
+                    logBuilder.WriteTo.File(new CompactJsonFormatter(), logPath, fileSizeLimitBytes: 5 * 1000^2)
+                        .WriteTo.Console(LogEventLevel.Error);
+#endif
+                }
+            );
+
+        return builder;
+    }
+
+    private static TemplateTheme GetTheme() => new(TemplateTheme.Code,
+        new Dictionary<TemplateThemeStyle, string>
+        {
+            [TemplateThemeStyle.SecondaryText] = "\e[0;97m",
+        }
+    );
 }
