ddf94d4fd1866da39b5f22af17b84ccdfc26ee7e
* <tbui17@users.noreply.github.com>
2025-05-02 21:12:10 -0600

    Fix share setup breaking in CI

--- test/IntellisenseTests/TestShare.cs
+++ test/IntellisenseTests/TestShare.cs
@@ -1,90 +1,44 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Management.Automation;
+using static SimpleExec.Command;
 
 namespace IntellisenseTests;
 
-public class TestShare : IDisposable
+public sealed class TestShare : IDisposable
 {
-    public DirectoryInfo Folder { get; set; }
-    public string Name { get; set; }
+    public DirectoryInfo Folder { get; }
+    public string Name { get; }
 
     public TestShare()
     {
-        // initialize folder
         var id = Guid.NewGuid();
-        var shareName = $"TestShare_{id}";
-        var shareFolder = Path.Combine(Path.GetTempPath(), shareName);
-        var folder = Directory.CreateDirectory(shareFolder);
-        Name = shareName;
-        Folder = folder;
+        Name = $"TestShare_{id}";
+        var shareFolder = Path.Combine(Path.GetTempPath(), Name);
+        Folder = Directory.CreateDirectory(shareFolder);
 
         try
         {
-            // initialize shares
-            using var ps = PowerShell.Create();
-            ps
-                .AddCommand("New-SmbShare")
-                .AddParameter("Name", Name)
-                .AddParameter("Path", Folder.FullName)
-                .AddParameter("FullAccess", "Everyone")
-                .Invoke();
-
-            if (ps.HadErrors)
-            {
-                var errs = GetErrors(ps);
-                throw new AggregateException(errs);
-            }
+            Run("net",
+                $"share {Name}=\"{Folder.FullName}\" /GRANT:Everyone,FULL",
+                noEcho: true
+            );
         }
-        catch (Exception)
+        catch (Exception ex)
         {
             Dispose();
-            throw;
+            throw new Exception($"Failed to create share {Name}", ex);
         }
     }
-
-    private static IEnumerable<Exception> GetErrors(PowerShell ps)
-    {
-        var errs = ps.Streams.Error.Select(x =>
-            {
-                if (x.CategoryInfo.Category is ErrorCategory.PermissionDenied)
-                {
-                    const string message =
-                        "Insufficient permissions. Did the executing session have administrator rights?";
-                    return new PSSecurityException(message, x.Exception);
-                }
-
-                if (x.Exception is null)
-                {
-                    return new Exception(x.ToString())
-                    {
-                        Data = { ["ErrorRecord"] = x }
-                    };
-                }
-
-                return x.Exception;
-            }
-        );
-        return errs;
-    }
-
 
     public void Dispose()
     {
         try
         {
-            using var ps = PowerShell
-                .Create()
-                .AddCommand("Remove-SmbShare")
-                .AddParameter("Name", Name)
-                .AddParameter("Force", true);
-            ps.Invoke();
+            Run("net", $"share {Name} /DELETE /Y", noEcho: true);
         }
         finally
         {
-            Folder.Delete(true);
+            Folder.Delete(recursive: true);
         }
     }
 }
