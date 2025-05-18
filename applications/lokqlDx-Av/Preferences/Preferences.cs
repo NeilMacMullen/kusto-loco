915f24fd5bacf6538e56b813591c1af80ecc57b5
Epat <i.am@epat.xyz>
2025-05-18 23:29:37 +0200

    Basic UI, querying, output in console and datagrid

---
+++ applications/lokqlDx-Av/Preferences/Preferences.cs
@@ -0,0 +1,39 @@
+﻿namespace LokqlDx;
+
+public class MruEntry
+{
+    public string Path { get; set; } = string.Empty;
+    public DateTime LastAccessed { get; set; } = DateTime.MinValue;
+}
+/// <summary>
+/// The list of recently used files/projects
+/// </summary>
+/// <remarks>
+/// This may be quite contended if the user has multiple copies of the application
+/// open
+/// </remarks>
+public class PersistedMruList
+{
+    public MruEntry [] RecentProjects { get; set; } = [];
+}
+
+/// <summary>
+/// User preferences that affect the UI
+/// </summary>
+/// <remarks>
+/// These are persisted to disk on application close and loaded at startup
+/// </remarks>
+public class UIPreferences
+{
+    public double FontSize { get; set; } = 20;
+    public string FontFamily { get; set; } = "Consolas";
+    public double WindowWidth { get; set; }
+    public double WindowHeight { get; set; }
+    public double WindowTop { get; set; }
+    public double WindowLeft { get; set; }
+    public bool WordWrap { get; set; } = false;
+    public bool ShowLineNumbers { get; set; } = false;
+    public string[] MainGridSerialization { get; set; } = [];
+    public string[] EditorGridSerialization { get; set; } = [];
+}
+
---
+++ applications/lokqlDx-Av/Views/MainView.axaml.cs
@@ -0,0 +1,48 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Threading;
+using LokqlDx.ViewModels;
+using SkiaSharp;
+using Topten.RichTextKit;
+
+namespace LokqlDx.Views;
+
+public partial class MainView : UserControl, IDisposable
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnDataContextChanged(EventArgs e)
+    {
+        base.OnDataContextChanged(e);
+        if (DataContext is MainViewModel mvm)
+        {
+            mvm.PropertyChanged += Mvm_PropertyChanged;
+        }
+    }
+
+    private void Mvm_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        if (DataContext is MainViewModel mvm)
+        {
+            if (e.PropertyName == nameof(MainViewModel.ColumnDefinitions) && mvm.ColumnDefinitions is not null)
+            {
+                MainGrid.ColumnDefinitions = mvm.ColumnDefinitions;
+            }
+            else if (e.PropertyName == nameof(MainViewModel.RowDefinitions) && mvm.RowDefinitions is not null)
+            {
+                MainGrid.RowDefinitions = mvm.RowDefinitions;
+            }
+        }
+    }
+
+    public void Dispose()
+    {
+        if (DataContext is MainViewModel mvm)
+        {
+            mvm.PropertyChanged -= Mvm_PropertyChanged;
+        }
+    }
+}
