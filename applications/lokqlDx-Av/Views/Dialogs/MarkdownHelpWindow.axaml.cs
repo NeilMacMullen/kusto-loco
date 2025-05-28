d2248801fc0177c1c8eb7e92a9d452aed201a2ef
NeilMacMullen <neil.macmullen@outlook.com>
2025-05-28 19:21:57 +0100

    Update to latest markdownviewer and remove workaround code for codeblocks

--- applications/lokqlDx-Av/Views/Dialogs/MarkdownHelpWindow.axaml.cs
+++ applications/lokqlDx-Av/Views/Dialogs/MarkdownHelpWindow.axaml.cs
@@ -1,8 +1,4 @@
-﻿using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Controls.Documents;
-using Avalonia.Media;
-
+﻿using Avalonia.Controls;
 
 namespace LokqlDx.Views.Dialogs;
 
@@ -11,33 +7,5 @@ public partial class MarkdownHelpWindow : UserControl
     public MarkdownHelpWindow()
     {
         InitializeComponent();
-    }
-
-    private void MarkdownViewer_LayoutUpdated(object? sender, EventArgs e)
-    {
-        if (MarkdownViewer.Content is Panel panel)
-            foreach (var child in panel.Children)
-            {
-                if (child is TextBlock tb)
-                    foreach (var inline in tb.Inlines ?? [])
-                        if (inline is InlineUIContainer iuc && iuc.Child is Border br)
-                            ColorBorder(br);
-
-                if (child is Grid grid)
-                    foreach (var c in grid.Children ?? [])
-                        if (c is Border br)
-                            ColorBorder(br);
-
-                if (child is Border b) ColorBorder(b);
-            }
-    }
-
-    private void ColorBorder(Border br)
-    {
-        var backgroundBrush = this.FindResource("SystemControlBackgroundBaseMediumBrush") as IBrush;
-        var borderBrush = this.FindResource("SystemControlTransientBorderBrush") as IBrush;
-        br.Background = backgroundBrush;
-        br.BorderThickness = new Thickness(1.5);
-        br.BorderBrush = borderBrush;
     }
 }
