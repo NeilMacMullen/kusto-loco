890b1e2013e669a3905468ce62bdc0121aec8f82
* <tbui17@users.noreply.github.com>
2025-07-02 16:31:37 -0600

    Document completion window behavior

--- libraries/lokqlDxComponents/Views/Dialogs/CompletionManager.cs
+++ libraries/lokqlDxComponents/Views/Dialogs/CompletionManager.cs
@@ -50,8 +50,24 @@ public class CompletionManager : IDisposable
             Rewind = 0,
             OnCompletionWindowDataPopulated = completionWindow =>
             {
-                // since we could be starting in the middle of a path name, we need to adjust the start offset to account for the extant fragment of the name
+                // https://github.com/AvaloniaUI/AvaloniaEdit/blob/c8b0d481ec65eaebec2167165e79f62c363462b7/src/AvaloniaEdit/CodeCompletion/CompletionWindow.cs#L224-L257
+                // https://github.com/AvaloniaUI/AvaloniaEdit/blob/c8b0d481ec65eaebec2167165e79f62c363462b7/src/AvaloniaEdit/CodeCompletion/CompletionList.cs#L336
+                // Avalon(ia)Edit's CompletionWindow behavior is mostly independent of our code once the completion window is open.
+                // Its internal filtering and selection algorithm is not a simple Contains or StartsWith. It calculates a quality score based on a prioritized list of string comparison strategies.
+                // It is also transitively dependent on 2 variables with different lifecycles:
+                // 1. The starting offset of the texteditor's caret when the CompletionWindow was first opened
+                // 2. Its current offset of the texteditor's caret as the user types
+                // Therefore, we need to be cognizant when implementing features related to the caret position.
+
+
+                // This ensures consistent completion behavior when we lose focus and resume typing for paths.
+                // Since path completion is unique in that path completion can start from a letter (e.g. "/myFol" => "/myFold"), we need to ensure that not only does the insertion "rewind" consistently,
+                // the filtering algorithm also produces consistent results as we traverse through the lifecycles of both the starting offset and the current offset.
+                // Typing "/myFold" should produce the same list of available results as typing "/myFol" (focus loss) + "d".
+                // For every key stroke in "/myFolder", its result should be the same as doing the same, but pressing "ESC" before the next keystroke.
+                // Since we already adjusted the cursor's starting offset, we do not need to add any additional "rewinding" in the ICompletionData we pass to the completion window.
                 completionWindow.StartOffset = _textEditor.CaretOffset - result.Filter.Length;
+
 
                 // when editor loses focus mid-path and user resumes typing, it won't require a second keypress to select the relevant result
                 completionWindow.CompletionList.SelectItem(result.Filter);
