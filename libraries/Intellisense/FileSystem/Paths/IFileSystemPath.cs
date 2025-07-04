0e7e1a6e36d88a828b95c6c069e6f653e05705ba
* <tbui17@users.noreply.github.com>
2025-07-04 11:24:21 -0600

    refactor reduce FileSystemPath scope
    - Needs unit tests
    - Need to review design before exposing outside of module

--- libraries/Intellisense/FileSystem/Paths/IFileSystemPath.cs
+++ libraries/Intellisense/FileSystem/Paths/IFileSystemPath.cs
@@ -1,10 +1,17 @@
 ﻿namespace Intellisense.FileSystem.Paths;
 
-public abstract class FileSystemPath(string value)
+public abstract class FileSystemPath
 {
-    public virtual string Value => value;
+    private readonly string _value;
+
+    internal FileSystemPath(string value)
+    {
+        _value = value;
+    }
+    public virtual string Value => _value;
     public abstract bool IsRootDirectory { get; }
-    public virtual string ParentPath => Path.GetDirectoryName(value) ?? string.Empty;
-    public virtual bool EndsWithDirectorySeparator => value.EndsWithDirectorySeparator();
+    public virtual string ParentPath => Path.GetDirectoryName(_value) ?? string.Empty;
+    public virtual bool EndsWithDirectorySeparator => _value.EndsWithDirectorySeparator();
     public virtual bool IsEmpty => this is EmptyPath;
+    public static FileSystemPath Empty => EmptyPath.Instance;
 }
