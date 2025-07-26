3e680456d95759a9363c97c9f9bb5df28eac1720
NeilMacMullen <neil.macmullen@outlook.com>
2025-07-26 13:21:55 +0100

    clean up names

--- templates/rebuild-templates.ps1
+++ templates/rebuild-templates.ps1
@@ -1,11 +1,14 @@
 
-textrude.exe render --models Csv!model=Types.csv --template IndexFinder.sbn --output output=..\libraries\KustoLoco.Core\Evaluation\BuiltIns\Aggregates\IndexFinder.cs --verbose
+function extrude($sbn,$path)
+{
+write-host "building '$sbn.cs' in '$path'"
+$cmd ="textrude.exe render --models Csv!model=Types.csv --template $sbn.sbn --output output=..\libraries\KustoLoco.Core\Evaluation\BuiltIns\$path\$sbn.cs --verbose"
+write-host "Executing '$cmd'"
+invoke-expression  $cmd
+}
 
-textrude.exe render --models Csv!model=Types.csv --template argmaxmin.sbn --output output=..\libraries\KustoLoco.Core\Evaluation\BuiltIns\Aggregates\ArgMinMaxFunctionImpl.cs --verbose
-
-textrude.exe render --models Csv!model=Types.csv --template takeAny.sbn --output output=..\libraries\KustoLoco.Core\Evaluation\BuiltIns\Aggregates\TakeAny.cs --verbose
-
-textrude.exe render --models Csv!model=Types.csv --template TypeComparison.sbn --output output=..\libraries\KustoLoco.Core\Evaluation\BuiltIns\ScalarFunctions\TypeComparison.cs --verbose
-
- textrude.exe render --models Csv!model=C:\work\open_source\kusto-loco\templates\Types.csv --template C:\work\open_source\kusto-loco\templates\MaxMinOf.sbn --output output=C:\work\open_source\kusto-loco\libraries\KustoLoco.Core\Evaluation\BuiltIns\ScalarFunctions\MinMaxOfFunctionImpl.cs --verbose
-
+extrude IndexFinder Aggregates
+extrude ArgMinMaxFunctionImpl Aggregates
+extrude TakeAny Aggregates
+extrude TypeComparison ScalarFunctions
+extrude MinMaxOfFunctionImpl ScalarFunctions
