f36bbb5f9f4c471409cdab91f452c749617c40c2
David Lloyd <david.lloyd@sensize.net>
2025-11-18 13:46:44 +0000

    Fix invalid column index value for ColumnResult.Empty

--- libraries/KustoLoco.Core/ColumnResult.cs
+++ libraries/KustoLoco.Core/ColumnResult.cs
@@ -4,5 +4,5 @@ namespace KustoLoco.Core;
 
 public readonly record struct ColumnResult(string Name, int Index, Type UnderlyingType)
 {
-    public static readonly ColumnResult Empty = new(string.Empty, 0, typeof(string));
+    public static readonly ColumnResult Empty = new(string.Empty, -1, typeof(string));
 }
