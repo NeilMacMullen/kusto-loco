ad9a9c25d1a6f8b6c02b115631ccf069752237ac
NeilMacMullen <neil.macmullen@outlook.com>
2025-12-13 15:44:56 +0000

    Make keydown as handled when copying from datagrid

--- applications/lokqlDx/Views/RenderingSurfaceView.axaml.cs
+++ applications/lokqlDx/Views/RenderingSurfaceView.axaml.cs
@@ -2,6 +2,7 @@ using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 using LokqlDx.ViewModels;
+
 #pragma warning disable VSTHRD100
 
 namespace LokqlDx.Views;
@@ -23,31 +24,35 @@ public partial class RenderingSurfaceView : UserControl, IDisposable
         DataGrid.KeyDown += DataGrid_KeyDown;
     }
 
-    private async void DataGrid_KeyDown(object? sender, Avalonia.Input.KeyEventArgs e)
+    private async void DataGrid_KeyDown(object? sender, KeyEventArgs e)
     {
         var vm = DataGrid.DataContext as RenderingSurfaceViewModel;
         if (vm != null)
-        {
             if ((e.KeyModifiers & KeyModifiers.Control) != 0)
             {
                 if (e.Key == Key.C)
                 {
-                     await vm.DataGridCopyCommand.ExecuteAsync("cell");
+                    await vm.DataGridCopyCommand.ExecuteAsync("cell");
+                    e.Handled = true;
                 }
+
                 if (e.Key == Key.R)
                 {
                     await vm.DataGridCopyCommand.ExecuteAsync("row");
+                    e.Handled = true;
                 }
+
                 if (e.Key == Key.L)
                 {
                     await vm.DataGridCopyCommand.ExecuteAsync("column");
+                    e.Handled = true;
                 }
+
                 if (e.Key == Key.A)
                 {
                     await vm.DataGridCopyCommand.ExecuteAsync("table");
+                    e.Handled = true;
                 }
             }
-
-        }
     }
 }
