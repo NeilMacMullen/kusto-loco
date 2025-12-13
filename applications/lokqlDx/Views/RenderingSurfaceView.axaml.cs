df73ea645ecdaf7abbe967222b499b3e89bcafc7
Neil MacMullen <neil.macmullen@outlook.com>
2025-12-13 11:37:57 +0000

    Support ctrl=c copying from datagrid

--- applications/lokqlDx/Views/RenderingSurfaceView.axaml.cs
+++ applications/lokqlDx/Views/RenderingSurfaceView.axaml.cs
@@ -1,7 +1,8 @@
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 using LokqlDx.ViewModels;
-using Vanara.PInvoke;
+#pragma warning disable VSTHRD100
 
 namespace LokqlDx.Views;
 
@@ -19,9 +20,34 @@ public partial class RenderingSurfaceView : UserControl, IDisposable
     private void InitializeComponent()
     {
         AvaloniaXamlLoader.Load(this);
+        DataGrid.KeyDown += DataGrid_KeyDown;
     }
 
+    private async void DataGrid_KeyDown(object? sender, Avalonia.Input.KeyEventArgs e)
+    {
+        var vm = DataGrid.DataContext as RenderingSurfaceViewModel;
+        if (vm != null)
+        {
+            if ((e.KeyModifiers & KeyModifiers.Control) != 0)
+            {
+                if (e.Key == Key.C)
+                {
+                     await vm.DataGridCopyCommand.ExecuteAsync("cell");
+                }
+                if (e.Key == Key.R)
+                {
+                    await vm.DataGridCopyCommand.ExecuteAsync("row");
+                }
+                if (e.Key == Key.L)
+                {
+                    await vm.DataGridCopyCommand.ExecuteAsync("column");
+                }
+                if (e.Key == Key.A)
+                {
+                    await vm.DataGridCopyCommand.ExecuteAsync("table");
+                }
+            }
 
-
-    
+        }
+    }
 }
