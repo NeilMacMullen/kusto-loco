ce543b166ae24972e64ba4936615bdb0a3981416
Neil MacMullen <neil.macmullen@outlook.com>
2026-01-17 17:10:18 +0000

    try to fix up test

--- test/IntellisenseTests/FileSystemIntellisenseServiceHostCompletionTests.cs
+++ test/IntellisenseTests/FileSystemIntellisenseServiceHostCompletionTests.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using AwesomeAssertions;
 using Intellisense.FileSystem;
@@ -12,10 +13,22 @@ namespace IntellisenseTests;
 
 public class FileSystemIntellisenseServiceHostCompletionTests : IAsyncLifetime
 {
-    private IFileSystemIntellisenseService _service = null!;
+    private IFileSystemIntellisenseService? _service;
+
+    private static bool ShouldSkip() =>
+        !PlatformHelper.IsCi() ||
+        !RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ||
+        !PlatformHelper.IsWindowsAdmin();
 
     public async Task InitializeAsync()
     {
+        // Skip initialization if tests will be skipped anyway
+        // This prevents stack overflow from Win32 API calls in unsupported environments
+        if (ShouldSkip())
+        {
+            return;
+        }
+
         var provider = new IntegrationTestContainer();
         var service = provider.GetRequiredService<IFileSystemIntellisenseService>();
         var result = await service.GetPathIntellisenseOptionsAsync("//");
@@ -29,19 +42,28 @@ public class FileSystemIntellisenseServiceHostCompletionTests : IAsyncLifetime
         _service = service;
     }
 
-    public async Task DisposeAsync() => await RunAsync("net", $@"use \\{IPAddress.Loopback} /delete", noEcho: true);
+    public async Task DisposeAsync()
+    {
+        // Only cleanup if we actually initialized
+        if (_service is null)
+        {
+            return;
+        }
+
+        await RunAsync("net", $@"use \\{IPAddress.Loopback} /delete", noEcho: true);
+    }
 
     [WindowsAdminCiFact]
     public async Task GetPathIntellisenseOptions_TwoSlash_ShowsServersFromPersistedShareConnections()
     {
-        var result = await _service.GetPathIntellisenseOptionsAsync("//");
+        var result = await _service!.GetPathIntellisenseOptionsAsync("//");
         result.Entries.Should().ContainSingle(x => x.Name == IPAddress.Loopback.ToString());
     }
 
     [WindowsAdminCiFact]
     public async Task GetPathIntellisenseOptions_PartialHost_ShowsServersFromPersistedShareConnectionsWithFilter()
     {
-        var result = await _service.GetPathIntellisenseOptionsAsync("//172.");
+        var result = await _service!.GetPathIntellisenseOptionsAsync("//172.");
         result.Entries.Should().ContainSingle(x => x.Name == IPAddress.Loopback.ToString());
         result.Filter.Should().Be("172.");
     }
